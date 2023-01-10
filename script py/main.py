abe98ea5433180d2641a98f3c39780b7ab293e11
Maxime Caparros <maxoucaparros@gmail.com>
2023-01-10 12:09:14 +0100

    Modification du script :)

--- script py/main.py
+++ script py/main.py
@@ -1,16 +1,11 @@
 import unittest
 
-class main:
-    def SimpleMath(a, b):
-        c = a + b
-        return c
-    pass
-
+class SimpleMath:
+    @staticmethod
+    def addition(a, b):
+        return a + b
 
 class TestSimpleMath(unittest.TestCase):
-    def test_equal(self, d, a, b):
-        self.assertEqual(main.SimpleMath(a,b),d)
-
-    def test_isnull(self, a, b):
-        self.assertFalse(main.SimpleMath(a,b) == None)
-    pass
+    def test_addition(self):
+        result = SimpleMath.addition(1, 2)
+        self.assertEqual(result, 3)
